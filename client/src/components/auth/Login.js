f43f291425d194a3fe68d5265bc0c16e78a45111
ahmaat19@gmail.com <ahmaat19@gmail.com>
2020-09-23 10:01:52 +0300

    Getting Started with React & Frontend

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Fragment>
+      {/* <div className='alert alert-danger'>Invalid credentials</div> */}
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into Your Account
+      </p>
+      <form
+        className='form'
+        onSubmit={(e) => onSubmit(e)}
+        action='dashboard.html'
+      >
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+            placeholder='Password'
+            name='password'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
